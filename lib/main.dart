1b45ffe61b11a0f910d0736a05ecea69b6fde5ff
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 18:09:53 +0900

    縦と横のスクロール

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
 void main() {
   const columns = [ 
-    DataColumn( label: Text('商品名'), ),
+    //DataColumn( label: Text('商品名'), ),
+    DataColumn(
+      label: SizedBox(
+        width: 2000, // 横スクロール検証用
+        child: Text('商品名'),
+      )
+    ),
     DataColumn( label: Text('値段'), ),
   ];
+    
+  final List<DataRow> rows = [];
 
-  const rows = [
-    DataRow(
-      cells: [
-        DataCell(Text('アイテム1')),
-        DataCell(Text('300')),
-      ],
-    ),
-    DataRow(
-      cells: [
-        DataCell(Text('アイテム2')),
-        DataCell(Text('200')),
-      ],
-    ),
-    DataRow(
-      cells: [
-        DataCell(Text('アイテム3')),
-        DataCell(Text('100')),
-      ],
-    ),
-  ];
+  for(int i = 1; i <= 100; i++){
+    rows.add(
+      DataRow(
+        cells: [
+          DataCell(Text('アイテム$i')),
+          DataCell(Text('${i * 100}')),
+        ],
+      )
+    );
+  }
 
   final datatable = DataTable(
     columns: columns,
@@ -33,8 +31,15 @@ void main() {
 
   final body = Container( // ボディー
     padding: const EdgeInsets.all(30),
-    child: datatable
-  ); 
+    child: ListView( // 縦スクロール
+      children: [
+        SingleChildScrollView( // 横スクロール
+          scrollDirection: Axis.horizontal,
+          child: datatable,
+        )
+      ]
+    ),
+  );  
 
   final sc = Scaffold(
     body: SafeArea(child:body ,), // ボディー    
